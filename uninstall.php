a2aa5cd47fd034955b476716f6fa5e8b5c162230
Jean-Baptiste LUCCIONI <jean-baptiste@alhena-conseil.com>
2015-11-27 22:29:58 +0100

    del somes errors synthax

--- uninstall.php
+++ uninstall.php
@@ -1,8 +1,8 @@
 <?php
 // If uninstall is not called from WordPress, exit
-if (!defined('WP_UNINSTALL_PLUGIN')) {
-    exit();
-}
+// if (!defined('WP_UNINSTALL_PLUGIN')) {
+//     exit();
+// }
 
 global $wpdb;
 
