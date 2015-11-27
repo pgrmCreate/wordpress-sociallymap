3fcf6afbfc9594002932e832f4bcbddcfa2d2cfb
Jean-Baptiste LUCCIONI <jean-baptiste@alhena-conseil.com>
2015-11-27 22:04:32 +0100

    del somes errors synthax

--- assets/js/modal-manager.js
+++ assets/js/modal-manager.js
@@ -1,23 +1,20 @@
 (function($) {
-  $(document).ready(function(){
-	if ($('.sm-readmore-link').attr('data-display-type') == "modal") {
-		$('.sm-readmore-link').removeAttr('target');
-		$(".sm-display-modal").fancybox({
-			maxWidth	: 1900,
-			maxHeight	: 1200,
-			fitToView	: false,
-			width		: '90%',
-			height		: '80%',
-			autoSize	: false,
-			closeClick	: false,
-			openEffect	: 'none',
-			closeEffect	: 'none'
-		});
+    $(document).ready(function(){
+		if ($('.sm-readmore-link').attr('data-display-type') == "modal") {
+			$('.sm-readmore-link').removeAttr('target');
+			$(".sm-display-modal").fancybox({
+				maxWidth	: 1900,
+				maxHeight	: 1200,
+				fitToView	: false,
+				width		: '90%',
+				height		: '80%',
+				autoSize	: false,
+				closeClick	: false,
+				openEffect	: 'none',
+				closeEffect	: 'none'
+			});
 		} else {
 			$('.sm-readmore-link').removeAttr('data-fancybox-type');
 		}
-	});
-	
-
- });
+   });
 }(jQuery));
--- uninstall.php
+++ uninstall.php
@@ -4,8 +4,4 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
     exit();
 }
 
-global $wpdb;
 
-$wpdb->query("DROP TABLE IF EXISTS".$wpdb->prefix."sm_options");
-$wpdb->query("DROP TABLE IF EXISTS".$wpdb->prefix."sm_entity_options");
-$wpdb->query("DROP TABLE IF EXISTS".$wpdb->prefix."sm_entities");
