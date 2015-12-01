ea419e9997511bdf2b9e30147416cc1b5960e3f5
Jean-Baptiste LUCCIONI <jean-baptiste@alhena-conseil.com>
2015-12-01 15:07:36 +0100

    remove echo debug

--- includes/Requester.php
+++ includes/Requester.php
@@ -4,7 +4,6 @@ class Requester
 {
     public function launch($entityId, $token, $environement)
     {
-        error_log('#####################', 3, plugin_dir_path(__FILE__)."../logs/error.log");
         if (!is_callable('curl_init')) {
             error_log("Curl no exist, request impossible..\n", 3, plugin_dir_path(__FILE__)."../logs/error.log");
             header("HTTP/1.0 501 Not Implemented");
@@ -47,7 +46,7 @@ class Requester
 
             // Decode the JSON response
             $result = json_decode($result);
-            error_log('Result : '.$result, 3, plugin_dir_path(__FILE__)."../logs/error.log");
+            error_log('Result : '.print_r($result, true), 3, plugin_dir_path(__FILE__)."../logs/error.log");
 
 
             // The request failed
