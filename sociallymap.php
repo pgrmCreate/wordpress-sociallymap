63caee1c1c7b2892fd3ad25c1e54020bc63f4013
Jean-Baptiste LUCCIONI <jean-baptiste@alhena-conseil.com>
2015-12-02 12:10:49 +0100

    post name

--- sociallymap.php
+++ sociallymap.php
@@ -75,7 +75,7 @@ class SociallymapPlugin
 
         echo("<script> alert('ID: $postObject->ID'); </script>");
 
-        if (is_page([$postObject->ID])) {
+        if ($postObject->post_name) {
             echo("<script> alert('Bonne page!!!'); </script>");
         }
 
