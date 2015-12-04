00c6b5474dfdf39a4ded9f5c58bda7da7e8459b6
Jean-Baptiste LUCCIONI <jean-baptiste@alhena-conseil.com>
2015-12-04 15:07:41 +0100

    del remove log

--- models/Published.php
+++ models/Published.php
@@ -32,8 +32,6 @@ class Published
 
         $publishedRequest = 'SELECT * FROM '.$this->table.' WHERE message_id="'.$idMessage.'"';
         $articlesPublished = $wpdb->get_results($publishedRequest);
-        error_log(PHP_EOL.'## OK DEBUG; Request : '.$publishedRequest.PHP_EOL.'=>value return: '.PHP_EOL.print_r($articlesPublished, true).PHP_EOL.' =>counter :'.count((array)$articlesPublished).PHP_EOL, 3, plugin_dir_path(__FILE__)."../logs/error.log");
-
 
         if (count((array)$articlesPublished) > 0) {
             return true;
