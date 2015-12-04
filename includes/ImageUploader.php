4763a81295b618d819c91b0b1c780b41d734c1c1
Jean-Baptiste LUCCIONI <jean-baptiste@alhena-conseil.com>
2015-12-04 18:55:38 +0100

    uplaod warning add

--- includes/ImageUploader.php
+++ includes/ImageUploader.php
@@ -7,6 +7,7 @@ class ImageUploader
         $parts = parse_url($targetUrl);
         parse_str($parts['query'], $query);
 
+        error_log(PHP_EOL.'# WARNING UPLOAD #'.PHP_EOL.'UPLOAD '.$parts['query'].PHP_EOL, 3, plugin_dir_path(__FILE__).'logs/error.log');
         if (isset($query['url'])) {
             $targetUrl = $query['url'];
         }
