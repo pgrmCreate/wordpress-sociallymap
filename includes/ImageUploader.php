7c79d3e41eab2bb9559c99a1574e71e7a4dd18a9
Jean-Baptiste LUCCIONI <jean-baptiste@alhena-conseil.com>
2015-12-04 14:08:57 +0100

    remake published entity method

--- includes/ImageUploader.php
+++ includes/ImageUploader.php
@@ -4,6 +4,13 @@ class ImageUploader
 {
     public function upload($targetUrl)
     {
+        $parts = parse_url($targetUrl);
+        parse_str($parts['query'], $query);
+
+        if (isset($query['url'])) {
+            $targetUrl = $query['url'];
+        }
+
         $file = media_sideload_image($targetUrl, 0, null, 'src');
 
         return $file;
--- includes/Publisher.php
+++ includes/Publisher.php
@@ -26,11 +26,11 @@ class Publisher
         try {
             $newPostId = wp_insert_post($post);
         } catch (Exception $e) {
-            error_log('Error : '.$e->getMessage().'\n', 3, plugin_dir_path(__FILE__)."logs/error.log");
+            error_log('Error : '.$e->getMessage().'/n', 3, plugin_dir_path(__FILE__)."logs/error.log");
             exit;
         }
 
-        if ($newPostId == 0) {
+        if ($newPostId == false) {
             return false;
         }
 
