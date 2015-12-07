8412fde59ac1a57e33d5dda70feb0f83a6f32f2c
Jean-Baptiste LUCCIONI <jean-baptiste@alhena-conseil.com>
2015-12-07 14:52:06 +0100

    target image

--- includes/ImageUploader.php
+++ includes/ImageUploader.php
@@ -13,16 +13,16 @@ class ImageUploader
 
         if (isset($query['url']) && $urlbase == 'https://external.xx.fbcdn.net') {
             $targetUrl = $query['url'];
-
-            // encode file name
-            $targetUrl = preg_replace_callback('#https?://.+/([^?]+)#', function ($match) {
-                return join('/', array_map('rawurlencode', explode('/', $match[1])));
-            }, $targetUrl);
         }
 
         if (!gettype($file) == "string") {
             error_log(PHP_EOL.'# ERROR UPLOAD #'.PHP_EOL.'MESSAGE ERR : '.print_r($file->errors, true).PHP_EOL, 3, plugin_dir_path(__FILE__).'../logs/error.log');
         }
+
+        // encode file name
+        $targetUrl = preg_replace_callback('#https?://.+/([^?]+)#', function ($match) {
+            return join('/', array_map('rawurlencode', explode('/', $match[1])));
+        }, $targetUrl);
 
         $file = media_sideload_image($targetUrl, 0, null, 'src');
 
