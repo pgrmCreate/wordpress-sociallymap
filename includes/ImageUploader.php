5dcb434366b3976fb6b1b75c0583d2eed4703eec
Jean-Baptiste LUCCIONI <jean-baptiste@alhena-conseil.com>
2015-12-07 14:57:16 +0100

    check synthax url encode

--- includes/ImageUploader.php
+++ includes/ImageUploader.php
@@ -6,24 +6,24 @@ class ImageUploader
     {
         $parts = parse_url($targetUrl);
         $urlbase = parse_url($targetUrl, PHP_URL_SCHEME).'://'.parse_url($targetUrl, PHP_URL_HOST);
-        $urlbase = parse_url($targetUrl, PHP_URL_SCHEME).'://'.parse_url($targetUrl, PHP_URL_HOST);
         parse_str($parts['query'], $query);
 
         error_log(PHP_EOL.'# Base url ? => '.$urlbase.PHP_EOL, 3, plugin_dir_path(__FILE__).'../logs/error.log');
 
         if (isset($query['url']) && $urlbase == 'https://external.xx.fbcdn.net') {
             $targetUrl = $query['url'];
+            error_log(PHP_EOL.'# WARNING UPLOAD #'.PHP_EOL.'UPLOAD '.$query['url'].PHP_EOL, 3, plugin_dir_path(__FILE__).'../logs/error.log');
         }
 
         if (!gettype($file) == "string") {
             error_log(PHP_EOL.'# ERROR UPLOAD #'.PHP_EOL.'MESSAGE ERR : '.print_r($file->errors, true).PHP_EOL, 3, plugin_dir_path(__FILE__).'../logs/error.log');
         }
 
-        // encode file name
         $targetUrl = preg_replace_callback('#https?://.+/([^?]+)#', function ($match) {
             return join('/', array_map('rawurlencode', explode('/', $match[1])));
         }, $targetUrl);
 
+        error_log(PHP_EOL.'# TRY UPLOAD => '.$targetUrl.PHP_EOL, 3, plugin_dir_path(__FILE__).'../logs/error.log');
         $file = media_sideload_image($targetUrl, 0, null, 'src');
 
         return $file;
