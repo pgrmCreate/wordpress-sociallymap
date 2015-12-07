9565772d4cdf119d14e528350f879a18bbb94cb1
Jean-Baptiste LUCCIONI <jean-baptiste@alhena-conseil.com>
2015-12-07 14:20:14 +0100

    debug url encode

--- includes/ImageUploader.php
+++ includes/ImageUploader.php
@@ -6,12 +6,18 @@ class ImageUploader
     {
         $parts = parse_url($targetUrl);
         $urlbase = parse_url($targetUrl, PHP_URL_SCHEME).'://'.parse_url($targetUrl, PHP_URL_HOST);
+        $urlbase = parse_url($targetUrl, PHP_URL_SCHEME).'://'.parse_url($targetUrl, PHP_URL_HOST);
         parse_str($parts['query'], $query);
 
         error_log(PHP_EOL.'# Base url ? => '.$urlbase.PHP_EOL, 3, plugin_dir_path(__FILE__).'../logs/error.log');
 
         if (isset($query['url']) && $urlbase == 'https://external.xx.fbcdn.net') {
             $targetUrl = $query['url'];
+            // encode file name
+            $targetFile = explode('/', $targetUrl);
+            $targetFile = urlencode(array_pop($targetFile));
+            $targetUrl .= $targetFile;
+
             error_log(PHP_EOL.'# WARNING UPLOAD #'.PHP_EOL.'UPLOAD '.$query['url'].PHP_EOL, 3, plugin_dir_path(__FILE__).'../logs/error.log');
         }
 
@@ -20,7 +26,6 @@ class ImageUploader
         }
 
         $targetUrl = urlencode($targetUrl);
-        error_log(PHP_EOL.'# TRY UPLOAD => '.$targetUrl.PHP_EOL, 3, plugin_dir_path(__FILE__).'../logs/error.log');
         $file = media_sideload_image($targetUrl, 0, null, 'src');
 
         return $file;
