90db05edc302b39ab577912789e9317ea7a8d6ba
Jean-Baptiste LUCCIONI <jean-baptiste@alhena-conseil.com>
2016-01-21 11:46:39 +0100

    error del file pending update

--- test/exception/fileDownloadException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class fileDownloadException extends Exception
-{
-    public function __construct($message, $code = 0, Exception $previous = null)
-    {
-        parent::__construct($message, $code, $previous);
-    }
-}
