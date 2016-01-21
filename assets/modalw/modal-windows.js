4589ea2fcae589b737ffe9e6ff6688e1f90fca83
Jean-Baptiste LUCCIONI <jean-baptiste@alhena-conseil.com>
2016-01-21 11:27:13 +0100

    update modal display (return false for stop link)

--- assets/modalw/modal-windows.js
+++ assets/modalw/modal-windows.js
@@ -1,15 +1,15 @@
 (function($) {
     $(document).ready(function(){
         $(document).on('click', '.sm-readmore-link', function(e) {
-            e.preventDefault();
-            e.stopImmediatePropagation();
-            e.stopPropagation();
+            if ($(window).width() < 500) {
+               return true;
+            }
 
             var self = $(this);
 
             var href = self.attr('href');
 
-            var saveStyle = $('html').css('overflow');
+            saveStyle = $('html').css('overflow');
             $('html').css('overflow', 'hidden');
 
             var iframe = '<iframe class="modalw-iframe" src="'+href+'" id="modalw-sm-iframe"></iframe>';
@@ -17,7 +17,7 @@
 
             $('body').append(wrapper+'<div class="modalw-container"><p class="modalw-close">X</p>'+iframe+'</div>');
 
-            $('#modalw-sm-iframe').on('ready', function(){
+            $('#modalw-sm-iframe').load(function(){
                 $('.modalw-container').css('display', 'block');
                 $('.modalw-load-wrapper').remove();
 
@@ -28,36 +28,36 @@
                 });
             });
 
-            $(document).on('click', '.modalw-close, .modalw-wrapper', function (e) {
-                e.preventDefault();
-                e.stopImmediatePropagation();
-                e.stopPropagation();
-
-                $('.modalw-container').fadeOut('slow', function() {
-                    $(this).remove();
-                });
-                $('.modalw-wrapper').fadeOut('slow', function() {
-                    $(this).remove();
-                });
-                $('html').css('overflow', saveStyle);
-            });
-
-            function resizingWindow() {
-                // Get size of windows
-                var widthWindows = $(document).width();
-
-                // Calc size of modal (80% of windows)
-                var widthDynamic = widthWindows * 0.8;
-
-                // Set size of modal
-                $('.modalw-container').width(widthDynamic);
-
-                // calc margin left
-                var marginPicked = (widthWindows * 0.1)/1.3;
-
-                // set margin left to windows
-                $('.modalw-container').css('left', marginPicked+'px');
-            }
+            return false;
         });
+
+        $(document).on('click', '.modalw-close, .modalw-wrapper', function (e) {
+            $('.modalw-container').fadeOut('slow', function() {
+                $(this).remove();
+            });
+            $('.modalw-wrapper').fadeOut('slow', function() {
+                $(this).remove();
+            });
+            $('html').css('overflow', saveStyle);
+
+            return false;
+        });
+
+        function resizingWindow() {
+            // Get size of windows
+            var widthWindows = $(document).width();
+
+            // Calc size of modal (80% of windows)
+            var widthDynamic = widthWindows * 0.8;
+
+            // Set size of modal
+            $('.modalw-container').width(widthDynamic);
+
+            // calc margin left
+            var marginPicked = (widthWindows * 0.1)/1.3;
+
+            // set margin left to windows
+            $('.modalw-container').css('left', marginPicked+'px');
+        }
     });
 }(jQuery));
